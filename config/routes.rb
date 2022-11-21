05199cfbcd4440034502d993c7a4d303cbe947b2
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-11-21 14:56:29 +0000

    add_CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   get "/homes/about" => "homes#about", as: "about"
 end
