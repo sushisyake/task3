903a9d0f5521e3275462cc961cf598b67a4fbf79
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-11-21 13:16:42 +0000

    made_up_to_books_con

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
+  resources :books, only: [:new, :index, :show]
+  get "/homes/about" => "homes#about", as: "about"
+end
