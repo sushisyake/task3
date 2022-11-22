e6f64cda4b68448fa98787a77c5b0327a052c7e3
sushisyake <117505663+sushisyake@users.noreply.github.com>
2022-11-22 06:17:37 +0000

    made_function

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
   get "/homes/about" => "homes#about", as: "about"
 end
